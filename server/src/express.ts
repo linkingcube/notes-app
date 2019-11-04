ea3571e32137bcc2ca8b5272417f38ee1cecd72b
balhalees <bashar@slickspaces.com>
2019-11-03 20:28:58 -0800

    Added routes

--- server/src/express.ts
+++ server/src/express.ts
@@ -1,7 +1,21 @@
-import { Route } from 'tsoa';
-
+import { Body, Get, Post, Route } from 'tsoa';
+import { NotesController } from './controllers/note.controller';
+import { Note } from './models/note';
 
 @Route('Notes')
 export class NotesRouter {
 
+    constructor(private controller: NotesController = new NotesController()) {
+
+    }
+
+    @Get()
+    public async getAllNotes(): Promise<Note[]> {
+        return this.controller.getAllNotes();
+    }
+
+    @Post()
+    public async addNote(@Body() note: Pick<Note, 'title' | 'body'>): Promise<Note> {
+        return this.controller.addNote(note);
+    }
 }
