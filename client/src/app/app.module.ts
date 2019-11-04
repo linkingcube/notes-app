3d3b22eed2aa10bf1c216686ab559402d1c1ad5b
balhalees <bashar@slickspaces.com>
2019-11-03 21:49:03 -0800

    Added note editor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NoteComponent } from './note/note.component';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteComponent
+    NoteComponent,
+    NoteEditorComponent
   ],
   imports: [
     BrowserModule
