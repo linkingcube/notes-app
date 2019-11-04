35c3dc1c4e745839c20d58258528a6f34ba9d041
balhalees <bashar@slickspaces.com>
2019-11-03 22:45:50 -0800

    Added note services methods

--- client/src/app/note.service.spec.ts
+++ client/src/app/note.service.spec.ts
@@ -1,26 +1,59 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { NoteService } from './note.service';
 
 describe('NoteService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
     const service: NoteService = TestBed.get(NoteService);
+    httpMock.expectOne('http://localhost:3000/notes');
     expect(service).toBeTruthy();
+  });
+
+  it('should hydrate notes on load', () => {
+    const service: NoteService = TestBed.get(NoteService);
+    const req = httpMock.expectOne('http://localhost:3000/notes');
+    expect(service).toBeTruthy();
+    expect(req.request.method).toBe('GET');
   });
 
   it('should add note', () => {
     const service: NoteService = TestBed.get(NoteService);
-    service.notes.push({
+    httpMock.expectOne('http://localhost:3000/notes');
+    service.notes = [{
       title: 'note title',
       body: 'note body',
       id: 123,
       createdAt: (new Date()).toISOString(),
       updatedAt: (new Date()).toISOString()
-    });
+    }];
     expect(service.notes.length).toEqual(1);
   });
+
+  it('should post note', () => {
+    const service: NoteService = TestBed.get(NoteService);
+    const newNote = {
+      title: 'note title',
+      body: 'note body'
+    };
+    service.addNote(newNote);
+    let [get, post] = httpMock.match('http://localhost:3000/notes');
+    expect(get.request.method).toBe('GET');
+    expect(post.request.method).toBe('POST');
+    expect(post.request.body).toEqual(newNote);
+  });
 });
-
-
--- client/src/app/note.service.ts
+++ client/src/app/note.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Note } from './note';
 
 @Injectable({
@@ -9,10 +11,15 @@ export class NoteService {
 
   private notesSubject: BehaviorSubject<Note[]>;
   public notes$: Observable<Note[]>;
+  private baseUrl: string;
+  private notesPath: string;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.notesSubject = new BehaviorSubject<Note[]>([]);
     this.notes$ = this.notesSubject.asObservable();
+    this.baseUrl = environment.apiBaseUrl;
+    this.notesPath = environment.notesPath;
+    this.hydrate();
   }
 
   public get notes(): Note[] {
@@ -21,6 +28,23 @@ export class NoteService {
 
   public set notes(collection: Note[]) {
     this.notesSubject.next(collection);
+  }
+
+  public async addNote(newNote: Pick<Note, 'title' | 'body'>) {
+    const url = `${this.baseUrl}${this.notesPath}`;
+    const note = await this.http.post<Note>(url, newNote).toPromise();
+    this.notes = [
+      ...this.notes,
+      note
+    ].sort((a, b) => {
+      return a.updatedAt.valueOf() > b.updatedAt.valueOf() ? 1 : -1;
+    });
+  }
+
+  private async hydrate() {
+    const url = `${this.baseUrl}${this.notesPath}`;
+    const notes = await this.http.get<Note[]>(url).toPromise();
+    this.notes = notes;
   }
 
 }
